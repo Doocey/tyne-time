7b044c567c85cf8398f1e667a9f51f3c793bafd8
Doocey <doocey1@gmail.com>
2024-01-17 17:41:20 +0000

    Tidy up components, and build out homepage layout. Mass add pre-existing images with their legacy folder structure

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+export const SITE_TITLE: string = "Tyne Time";
+export const SITE_DESCRIPTION: string = `The latest Newcastle United news, reports, rumours, and opinions surrounding events at St. James' Park.`;
+
+export const HOMEPAGE_HERO_TITLE: string =
+  "Tyne Time - Occasional musings on Newcastle United";
