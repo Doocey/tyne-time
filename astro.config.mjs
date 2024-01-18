f4135503a2e21bbb643e3ed75aef25def3235cc9
Doocey <doocey1@gmail.com>
2024-01-18 17:23:16 +0000

    Introduce aliasing for components, layouts, and utils to clean up code.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.tynetime.com',
+  site: "https://www.tynetime.com",
   integrations: [mdx()],
-  compressHTML: true,
+  compressHTML: true
 });
