3e4e5a386308550f74638403ba908ec644a160e2
Doocey <doocey1@gmail.com>
2024-01-24 18:46:28 +0000

    Change build output folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,15 +6,7 @@ export default defineConfig({
   site: "https://www.tynetime.com",
   integrations: [mdx()],
   compressHTML: true,
-  vite: {
-    build: {
-      rollupOptions: {
-        output: {
-          entryFileNames: "entry.[hash].mjs",
-          chunkFileNames: "chunks/chunk.[hash].mjs",
-          assetFileNames: "assets/asset.[hash][extname]"
-        }
-      }
-    }
+  build: {
+    assets: "_assets"
   }
 });
