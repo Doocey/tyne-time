5f531ae9e45589a19224d0b7c21aa41ecd8bc9c1
Doocey <doocey1@gmail.com>
2024-01-27 15:44:06 +0000

    Remove MDX integration since we are only using standard Markdown, for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.tynetime.com",
-  integrations: [mdx()],
+  integrations: [],
   compressHTML: true,
   build: {
     assets: "_assets"
