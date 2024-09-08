6ed9d8734726f62b2ee218e320431b6bb10cf191
Doocey <doocey1@gmail.com>
2024-09-08 13:49:35 +0100

    Look at fixing lastBuildDate RSS quirkiness

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -13,24 +13,22 @@ export async function GET(context) {
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: posts
-      .map((post) => ({
-        ...post.data,
-        link: post.data.url,
-        pubDate: post.data.date,
-        ...(post.data.image && {
-          customData: `<media:content
+    items: posts?.map((post) => ({
+      ...post.data,
+      link: post.data.url,
+      pubDate: post.data.date,
+      ...(post.data.image && {
+        customData: `<media:content
           type="image/${post.data.image.format == "jpg" ? "jpeg" : "png"}"
           width="${post.data.image.width}"
           height="${post.data.image.height}"
           medium="image"
           url="https://www.tynetime.com${post.data.image.src}" />`
-        })
-      }))
-      .reverse(),
+      })
+    })),
     customData: [
       "<language>en-GB</language>",
-      `<lastBuildDate>${posts.at(-1).data.date.toUTCString()}</lastBuildDate>`,
+      `<lastBuildDate>${posts.at(0).data.date.toUTCString()}</lastBuildDate>`,
       `<atom:link href="https://www.tynetime.com/rss.xml" rel="self" type="application/rss+xml" />`
     ].join(" ")
   });
